c7d641b77021b023ee0866d02bc84c3098cf181b
djfrigid <electromagneticsnake@hotmail.co.uk>
2021-12-07 17:30:08 +0000

    Added Constant class for logger. Added PrintTimingData class for printing graduated intervals to logs

--- src/main/java/com/sparta/util/PrintTimingData.java
+++ src/main/java/com/sparta/util/PrintTimingData.java
@@ -5,7 +5,7 @@ import static com.sparta.util.Constants.LOGGER;
 public class PrintTimingData {
     public static void logTimingData(String message, long startTime, long endTime){
         if ((endTime-startTime) > 1000000){
-            LOGGER.info(message + (endTime-startTime) + " ms");
-        } else 
+            LOGGER.info(message + (endTime-startTime)/1000000 + " ms");
+        } else LOGGER.info(message + (endTime-startTime) + " ns");
     }
 }
