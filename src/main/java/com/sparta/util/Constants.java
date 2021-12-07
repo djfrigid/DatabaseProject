154b6024969504d2716f69667505d69b54a6ab10
djfrigid <electromagneticsnake@hotmail.co.uk>
2021-12-07 17:24:04 +0000

    temporary to pull£

--- src/main/java/com/sparta/util/Constants.java
+++ src/main/java/com/sparta/util/Constants.java
@@ -1,2 +1,8 @@
-package com.sparta.util;public class Constants {
+package com.sparta.util;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Constants {
+    public static final Logger LOGGER = LogManager.getLogger("Test name");
 }
--- src/main/java/com/sparta/util/PrintTimingData.java
+++ src/main/java/com/sparta/util/PrintTimingData.java
@@ -1,2 +1,11 @@
-package com.sparta.util;public class PrintTimingData {
+package com.sparta.util;
+
+import static com.sparta.util.Constants.LOGGER;
+
+public class PrintTimingData {
+    public static void logTimingData(String message, long startTime, long endTime){
+        if ((endTime-startTime) > 1000000){
+            LOGGER.info(message + (endTime-startTime) + " ms");
+        } else 
+    }
 }
