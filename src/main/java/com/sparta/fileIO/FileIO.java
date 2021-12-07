8e2b76ff7683e45473ae9db7a5a1eae1367e6be9
djfrigid <electromagneticsnake@hotmail.co.uk>
2021-12-07 08:35:26 +0000

    test

--- src/main/java/com/sparta/fileIO/FileIO.java
+++ src/main/java/com/sparta/fileIO/FileIO.java
@@ -1,2 +1,55 @@
-package com.sparta.fileIO;public class FileIO {
+package com.sparta.fileIO;
+
+import com.sparta.example.Employee;
+
+import java.io.*;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+
+public class FileIO {
+
+    public static void main(String[] args) {
+
+    }
+    private HashSet<Employee> uniqueEmployees = new HashSet<>();
+    private List<Employee> duplicatesAndCorrupted = new ArrayList<>();
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+
+    private static void readFromFile(String inputFilename, String outputFilename) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFilename)); BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilename))){
+            String nextLine;
+            reader.readLine(); // Read first line
+            while((nextLine = reader.readLine()) != null){
+                String[] components = nextLine.split(",");
+                int id = Integer.MAX_VALUE;
+                int salary = Integer.MIN_VALUE;
+                try{
+                    id = Integer.parseInt(components[0]);
+                    salary = Integer.parseInt(components[9]);
+                } catch (NumberFormatException nfe){
+                    nfe.printStackTrace();
+                }
+
+                String namePrefix = components[1];
+                String firstName = components[2];
+                char initial = components[3].charAt(0);
+                String lastName = components[4];
+                char gender = components[5].charAt(0);
+                String email = components[6];
+                Date dateOfBirth = (Date) FORMATTER.parse(components[7]);
+                Date dateOfJoining = (Date) FORMATTER.parse(components[7])
+                Employee newEmployee = new Employee(id, namePrefix, firstName, initial, lastName, gender, email, dateOfBirth, dateOfJoining, salary);
+                System.out.println(newEmployee);
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
 }
--- src/test/java/com/sparta/example/test/ValidationTest.java
+++ src/test/java/com/sparta/example/test/ValidationTest.java
@@ -1,2 +1,32 @@
-package com.sparta.example.test;public class ValidationTest {
+package com.sparta.example.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ValidationTest {
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"M", "F"})
+    public void genderValidation(String gender) {
+        assertTrue(validateGender(gender));
+    }
+
+    @Test
+    public void validationTestTwo() {
+    }
+
+    @Test
+    public void validationTestThree() {
+    }
 }
