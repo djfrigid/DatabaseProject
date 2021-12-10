0c1eb116f9b4ae9f15ed55d2901e6e37b9d59a43
djfrigid <electromagneticsnake@hotmail.co.uk>
2021-12-10 14:13:09 +0000

    changed batchSize to be from a variable in writer class. Best performance 128T/60s

--- src/main/java/com/sparta/model/dbaccess/MultithreadedDBWrites.java
+++ src/main/java/com/sparta/model/dbaccess/MultithreadedDBWrites.java
@@ -11,7 +11,7 @@ import java.util.concurrent.*;
 import static com.sparta.model.util.Constants.LOGGER;
 
 public class MultithreadedDBWrites {
-    private static final int poolSize = 20;
+    private static final int poolSize = 128;
 
     public static void writeNonDuplicatesOnly(Set<Employee> toWrite){
         ExecutorService pool = Executors.newFixedThreadPool(poolSize);
@@ -43,21 +43,22 @@ class DBWriter implements Runnable{
         EmployeeDao employeeDao = new EmployeeDaoImpl();
         int masterCount = 0;
         int batchCount = 0;
-        int numberOfBatches = 1;
+        int numberOfBatches = 0;
+        int batchSize = 256;
         Employee employee;
-        List<Employee> employeeBatch = new ArrayList<>(100);
+        List<Employee> employeeBatch = new ArrayList<>(batchSize);
         while((employee = chunk.poll()) != null){
             // employeeDao.insertEmployee(employee);
             employeeBatch.add(employee);
             masterCount+=1;
             batchCount+=1;
-            if (employeeBatch.size() > 256){
+            if (employeeBatch.size() == batchSize){
                 employeeDao.insertEmployeeBatch(employeeBatch);
                 batchCount=0;
                 numberOfBatches+=1;
                 employeeBatch.clear();
             }
-            if (masterCount % 256 == 0){
+            if (masterCount % batchSize == 0){
                 LOGGER.info("Thread " + Thread.currentThread().getId() + " has inserted " + numberOfBatches + " batches (" + masterCount +") records");
             }
         }
