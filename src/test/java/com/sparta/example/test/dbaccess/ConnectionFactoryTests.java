f21a6f3fea7109a6c37fb7f3e9f61d87459a7a31
talal1998 <thaheemtalal@gmail.com>
2021-12-10 11:04:26 +0000

    added initial connection and statement factory tests

--- src/test/java/com/sparta/example/test/dbaccess/ConnectionFactoryTests.java
+++
@@ -1,5 +0,0 @@
-package com.sparta.example.test.dbaccess;
-
-public class ConnectionFactoryTests {
-    
-}
---
+++ src/test/java/com/sparta/example/test/dbaccess/ConnectionTests.java
@@ -0,0 +1,42 @@
+package com.sparta.example.test.dbaccess;
+
+import com.sparta.dbaccess.ConnectionFactory;
+import com.sparta.dbaccess.StatementFactory;
+import com.sparta.util.DateFormatter;
+import com.sparta.validate.EmployeeValidate;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ConnectionTests {
+    Connection connection;
+
+    @BeforeEach
+    public void setUp() {
+        connection = ConnectionFactory.getConnection();
+    }
+
+    @Test
+    public void getConnectionTest() throws SQLException {
+        StatementFactory.getAllEmployees();
+        assertTrue(StatementFactory.getAllEmployees().isPoolable());
+    }
+
+    @Test
+    public void connectionCloseTest() throws SQLException {
+        StatementFactory.getAllEmployees();
+        ConnectionFactory.closeConnection();
+        assertTrue(StatementFactory.getAllEmployees().isClosed());
+    }
+
+}
