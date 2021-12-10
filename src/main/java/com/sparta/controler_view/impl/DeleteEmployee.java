457e824e79b41e1a901311422cb90e1221b7d9de
Ria622 <75190227+Ria622@users.noreply.github.com>
2021-12-10 15:25:19 +0000

    delete and insert

--- src/main/java/com/sparta/controler_view/impl/DeleteEmployee.java
+++ src/main/java/com/sparta/controler_view/impl/DeleteEmployee.java
@@ -1,6 +1,10 @@
 package com.sparta.controler_view.impl;
 
 import com.sparta.controler_view.io.bretty.console.view.ActionView;
+import com.sparta.model.dbaccess.EmployeeDao;
+import com.sparta.model.employee.Employee;
+
+import static com.sparta.model.util.Constants.EMPLOYEE_DAO;
 
 public class DeleteEmployee extends ActionView {
 
@@ -10,6 +14,10 @@ public class DeleteEmployee extends ActionView {
 
     @Override
     public void executeCustomAction() {
+        int id = prompt("Please enter employee ID: ", Integer.class);
+        //System.out.println("The employee deleted: " );
+        EMPLOYEE_DAO.deleteEmployee(id);
+
 
     }
 }
--- src/main/java/com/sparta/controler_view/impl/InsertEmployee.java
+++ src/main/java/com/sparta/controler_view/impl/InsertEmployee.java
@@ -1,6 +1,11 @@
 package com.sparta.controler_view.impl;
 
 import com.sparta.controler_view.io.bretty.console.view.ActionView;
+import com.sparta.model.employee.Employee;
+
+import java.sql.Date;
+
+import static com.sparta.model.util.Constants.EMPLOYEE_DAO;
 
 public class InsertEmployee extends ActionView {
 
@@ -11,7 +16,19 @@ public class InsertEmployee extends ActionView {
 
     @Override
     public void executeCustomAction() {
-
+        int id = prompt("Please enter employee ID: ", Integer.class);
+        String namePrefix = prompt("Please enter name prefix: ", String.class);
+        String firstName = prompt("Please enter first name", String.class);
+        char initial = prompt("Please enter an initial: ", String.class).charAt(0);
+        String lastName = prompt("Please enter last name: ", String.class);
+        char gender = prompt("Please enter Male(M) or Female(F): ", String.class).charAt(0);
+        String email = prompt("Please enter email: ", String.class);
+        Date dateofBirth = prompt("Please enter date of birth: ", Date.class);
+        Date dateOfJoining = prompt("Please enter joining date: ", Date.class);
+        int salary = prompt("Please enter employee salary: ", Integer.class);
+        Employee employee1 = new Employee(id,namePrefix,firstName,initial,lastName,
+                                gender, email,dateofBirth,dateOfJoining,salary);
+        EMPLOYEE_DAO.insertEmployee(employee1);
 
 
     }
